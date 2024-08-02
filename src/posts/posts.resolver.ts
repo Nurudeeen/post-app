741e475d2a4484f01465bdaca5d5636f72df54bf
Nurudeeen <nurudeenrabiu81@gmail.com>
2024-08-02 13:45:47 +0100

    (feat): add query service

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -1,4 +1,37 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { Post } from './models/post.model';
+import { PostsService } from './posts.service';
+import { CreatePostInput } from './dto/create-post.input';
 
-@Resolver()
-export class PostsResolver {}
+@Resolver(() => Post)
+export class PostsResolver {
+  constructor(private readonly postsService: PostsService) {}
+
+  @Query(() => [Post])
+  async posts() {
+    return this.postsService.findAll();
+  }
+
+  @Query(() => Post)
+  async post(@Args('id', { type: () => String }) id: string) {
+    return this.postsService.findOne(id);
+  }
+
+  @Mutation(() => Post)
+  async createPost(@Args('createPost') createPost: CreatePostInput) {
+    return this.postsService.create(createPost);
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Args('id', { type: () => String }) id: string,
+    @Args('updatePost') updatePost: CreatePostInput,
+  ) {
+    return this.postsService.update(id, updatePost);
+  }
+
+  @Mutation(() => Post)
+  async deletePost(@Args('id', { type: () => String }) id: string) {
+    return this.postsService.remove(id);
+  }
+}
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, InternalServerErrorException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePostInput } from './dto/create-post.input';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreatePostInput) {
+    try {
+      return await this.prisma.post.create({ data });
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to create post');
+    }
+  }
+
+  async findAll() {
+    return this.prisma.post.findMany();
+  }
+
+  async findOne(id: string) {
+    const post = await this.prisma.post.findUnique({ where: { id } });
+    if (!post) {
+      throw new NotFoundException(`Post with ID "${id}" not found`);
+    }
+    return post;
+  }
+
+  async update(id: string, data: CreatePostInput) {
+    const post = await this.findOne(id);
+    try {
+      return await this.prisma.post.update({
+        where: { id: post.id },
+        data,
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to update post');
+    }
+  }
+
+  async remove(id: string) {
+    const post = await this.findOne(id);
+    try {
+      return await this.prisma.post.delete({
+        where: { id: post.id },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException('Failed to delete post');
+    }
+  }
+}
